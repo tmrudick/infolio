53982bf1a1bd3044ea5ea14d42ec24d4400e6c39
Tom Rudick <tmrudick@gmail.com>
2012-05-19 09:56:37 -0700

    Adding default rails stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Infolio::Application.config.secret_token = '911eaf871133a958d65fa2512f709cb69994489f5945612739d9a864ac2ccea6f74cf1b2f669cc97bce00b34fa569338333c542e1c663bf49dc27d29ba780982'
