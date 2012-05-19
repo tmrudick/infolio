ba41e911b45cea89d720d7693c729d0780984573
Tom Rudick <tmrudick@gmail.com>
2012-05-19 13:34:17 -0700

    Moving the connected services page over to the website root

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,12 @@
 class HomeController < ApplicationController
-  before_filter :require_user, :only => [:loggedin]
+  before_filter :require_user
   
-  def index    
-  end
-  
-  def loggedin
+  def index
+    @service_hash = {}
     @services = Service.where('user_id = ?', current_user.id)
+    
+    for service in @services
+      @service_hash[service.name] = true
+    end
   end
 end
