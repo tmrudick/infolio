cd09eea4cfcad3f29d94d687ab3f9a32a3b02ba4
Quazie <Quazie@gmail.com>
2012-05-20 13:23:37 -0700

    real circles and twitter update

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -2,8 +2,8 @@
 # setting up twitter keys
 
 Twitter.configure do |config|
-  config.consumer_key = "huUubquOuyFutHVFECgYg"
-  config.consumer_secret = "cNd5uMj6goXAHiTUItRpx12N2pGqTF7p7VvEAa9TJWI"
-  config.oauth_token = "15384590-C4bLRFu2j6EVDiYXdjZiQqfhRXnU3JhWU5TkF3q2Z"
-  config.oauth_token_secret = "ihk3PWawoBn4wjfJBg69o4XQAQktJwH4IcbCy3kkR5k"
+  config.consumer_key = "PeQceGdREryhB4MmKwSdvQ"
+  config.consumer_secret = "KNGpK8nKbCosxpjyQ1TshRvrfYxrsd7N9VHOXpPCvg0"
+  config.oauth_token = "7389212-1AqTFKQYdpVoZ3A1ezSvlymWlvDexcec0Ibc1uglU"
+  config.oauth_token_secret = "k5KXShqejMZfjjJcJJ9EHMgWia24gMq7wRC2VEeeZiU"
 end
