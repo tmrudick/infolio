9af2d82ba01275d28c68ab882ae63a54d0f0fbd8
Kim Manis <kimmanis@gmail.com>
2012-05-20 16:24:56 -0700

    new twitter

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -2,8 +2,8 @@
 # setting up twitter keys
 
 Twitter.configure do |config|
-  config.consumer_key = "PeQceGdREryhB4MmKwSdvQ"
-  config.consumer_secret = "KNGpK8nKbCosxpjyQ1TshRvrfYxrsd7N9VHOXpPCvg0"
-  config.oauth_token = "7389212-1AqTFKQYdpVoZ3A1ezSvlymWlvDexcec0Ibc1uglU"
-  config.oauth_token_secret = "k5KXShqejMZfjjJcJJ9EHMgWia24gMq7wRC2VEeeZiU"
+  config.consumer_key = "iIhAoEEizCAGLGFrxQAU9Q"
+  config.consumer_secret = "oSIfsURVlZmBqrYXaxlSODi2vR8cozwHmqBloLOrWw"
+  config.oauth_token = "13709302-vsOmHfUwlXrPMHQpuqMxZJ2cSVMnBqCtd2pBp1Ks"
+  config.oauth_token_secret = "TYpPT3mYTF9F0SPlbMVqKA4aZmxG5Z7x1Y09BnOaqm4"
 end
