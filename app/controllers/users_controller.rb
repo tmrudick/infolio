a7543bc0e395866906c2096290b3acef18ea85e7
Kim Manis <kimmanis@gmail.com>
2012-05-19 17:45:12 -0700

    more tweets

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -60,7 +60,8 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.update_attributes(params[:user])
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+      	set_user(@user)
+        format.html { redirect_to dashboard_url, notice: 'User was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
