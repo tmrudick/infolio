9a969f5f46442c6a69e2f06f00969a4d8f5696aa
Kim Manis <kimmanis@gmail.com>
2012-05-20 15:50:25 -0700

    styling

--- app/assets/javascripts/d3.v2.js
+++ app/assets/javascripts/d3.v2.js
@@ -3010,8 +3010,8 @@ d3.scale.category20c = function() {
 };
 
 var d3_category10 = [
-  "#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd",
-  "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"
+  "#9d9d9d", "#00aeef", "#ccc"
+ 
 ];
 
 var d3_category20 = [
